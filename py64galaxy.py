9e99fb79f41e8c3377a29e830e4f126c68339d0c
Pasquale Rinaldi <pjrinaldi@gmail.com>
2012-12-02 18:36:53 -0500

    modified title

--- py64galaxy.py
+++ py64galaxy.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World')
+pygame.display.set_caption('Py64Galaxy')
 while True: # main game loop
     for event in pygame.event.get():
         if event.type == QUIT:
