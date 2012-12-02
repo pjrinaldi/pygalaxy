b2af73e2852100c4671795735290b8f3c51a3378
Pasquale Rinaldi <pjrinaldi@gmail.com>
2012-12-02 18:53:45 -0500

    initial title screen working.  need to add options

--- py64galaxy.py
+++ py64galaxy.py
@@ -4,7 +4,17 @@ from pygame.locals import *
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Py64Galaxy')
+
+LIGHTGRAY= (235, 235, 235)
+BLACK = (0, 0, 0)
+
+titleFont = pygame.font.Font('freesansbold.ttf', 32)
+titleText = titleFont.render('Python Galaxy', True, LIGHTGRAY)
+titleRect = titleText.get_rect()
+titleRect.center = (200, 50)
+
 while True: # main game loop
+    DISPLAYSURF.blit(titleText, titleRect)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
