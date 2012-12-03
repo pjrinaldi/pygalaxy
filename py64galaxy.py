8204ca33a194e3d429273d09d26f10acb429985f
Pasquale Rinaldi <pasquale@crunchbangmac.(none)>
2012-12-03 11:29:41 -0500

    figuring out how font works. now onto keyboard input and adding screen stuff

--- py64galaxy.py
+++ py64galaxy.py
@@ -2,19 +2,24 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+DISPLAYSURF = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Py64Galaxy')
 
 LIGHTGRAY= (235, 235, 235)
 BLACK = (0, 0, 0)
 
-titleFont = pygame.font.Font('freesansbold.ttf', 32)
+titleFont = pygame.font.Font(pygame.font.match_font('freesans', False, False), 72) 
 titleText = titleFont.render('Python Galaxy', True, LIGHTGRAY)
 titleRect = titleText.get_rect()
-titleRect.center = (200, 50)
+titleRect.center = (400, 100)
+startFont = pygame.font.Font(pygame.font.match_font('freesans', False, False), 48)
+startText = startFont.render('F1 - Start Single Player Game', True, LIGHTGRAY)
+startRect = startText.get_rect()
+startRect.center = (400, 200)
 
 while True: # main game loop
     DISPLAYSURF.blit(titleText, titleRect)
+    DISPLAYSURF.blit(startText, startRect)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
