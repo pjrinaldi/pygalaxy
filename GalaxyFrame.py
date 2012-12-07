3e777db9a1a3b446382fbc83337a5dc537eb4282
Pasquale Rinaldi <pjrinaldi@gmail.com>
2012-12-07 18:45:21 -0500

    working on doing bitmaps properly

--- GalaxyFrame.py
+++ GalaxyFrame.py
@@ -1,11 +1,12 @@
 from wx import wx
 import Image, ImageFont, ImageDraw
-import time
+# import time
 
 class GalaxyFrame(wx.Frame):
     
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(800, 600))
+        self.SetBackgroundColour(wx.Colour(0, 0, 0))
         self.imageViewer = wx.Panel(self, style=wx.BORDER_NONE)
         self.imageViewer.SetBackgroundColour(wx.Colour(0, 0, 0))
         self.imageViewer.Bind(wx.EVT_CHAR, self.CaptureKeys)
@@ -22,83 +23,56 @@ class GalaxyFrame(wx.Frame):
         self.configureGame = 0
         self.numPlayers = 0
         self.numWorlds = 0
-        
         self.mainImage = Image.new('L', self.GetSize())
         self.mainDraw = ImageDraw.Draw(self.mainImage)
         self.AddText(self.TitleText, self.TitleFont, self.mainDraw, 0)
-        self.BlitTextSurface(self.imageViewer, self.mainImage)
+        self.wxBmp = wx.EmptyBitmap(self.GetSize()[0], self.GetSize()[1])
+        self.mainBmp = wx.StaticBitmap(self.imageViewer, -1, self.wxBmp, wx.Point(0, 0))
+        self.BlitTextSurface(self.imageViewer, self.mainImage, self.mainBmp)
                 
         self.Show(True)
-
     def CaptureKeys(self, event):
         keycode = event.GetKeyCode()
         if self.configureGame == 0:
             self.keyStrokesList = []
             self.configureGame = 1
-            self.mainImage = Image.new('L', self.GetSize())
-            self.mainDraw = ImageDraw.Draw(self.mainImage)
             self.AddText(self.numPlayerText, self.gameFont, self.mainDraw, 0)
-            self.BlitTextSurface(self.imageViewer, self.mainImage)
+            self.BlitTextSurface(self.imageViewer, self.mainImage, self.mainBmp)
         elif self.configureGame == 1:
             if keycode > 47 and keycode < 58:
                 self.keyStrokesList.append(keycode - 48)
             elif keycode == 13:
                 testNum = ""
-                print self.keyStrokesList
                 for i in self.keyStrokesList:
                     testNum = testNum + str(i)
                 print testNum
-                print "Need to validate number and redisplay if not valid, otherwise continue on"
                 if int(testNum) > 0 and int(testNum) < 21:                    
                     self.configureGame = 2
-                    self.mainImage = Image.new('L', self.GetSize())
-                    self.mainDraw = ImageDraw.Draw(self.mainImage)
                     self.numPlayerText = self.numPlayerText + " " + testNum
                     self.AddText(self.numPlayerText, self.gameFont, self.mainDraw, 0)
                     self.AddText(self.numWorldsText, self.gameFont, self.mainDraw, 1)
-                    self.BlitTextSurface(self.imageViewer, self.mainImage)
+                    self.BlitTextSurface(self.imageViewer, self.mainImage, self.mainBmp)
                 else:
                     self.configureGame = 1
                     self.keyStrokesList = []
-                    self.imageViewer.Hide()
-                    # self.bmp.Hide()
-                    time.sleep(1)
-                    # self.bmp.Show()
                     self.imageViewer.SetBackgroundColour(wx.Colour(0, 0, 0))
-                    self.mainImage = Image.new('L', self.GetSize())
-                    self.mainDraw = ImageDraw.Draw(self.mainImage)
                     self.AddText(self.numPlayerText, self.gameFont, self.mainDraw, 0)
-                    self.BlitTextSurface(self.imageViewer, self.mainImage)
-        print keycode
+                    self.BlitTextSurface(self.imageViewer, self.mainImage, self.mainBmp)
         event.Skip()
 
-    def DisplayText(self, currentPanel, currentText, currentFont):
-        self.tmpText = Image.new('L', self.GetSize())
-        self.tmpDraw = ImageDraw.Draw(self.tmpText)
-        self.tmpSize = self.tmpDraw.textsize(currentText, font=currentFont)
-        self.tmpWidth = self.GetSizeTuple()[0]/2 - self.tmpSize[0]/2
-        self.tmpHeight = self.GetSizeTuple()[1]/2 - self.tmpSize[1]/2 - 50
-        self.tmpDraw.text((self.tmpWidth, self.tmpHeight), currentText, font=currentFont, fill=215)
-        self.ShowBitmapFromPIL(currentPanel, self.tmpText)
-        currentPanel.SetFocus()
-
     def AddText(self, currentText, currentFont, currentDraw, currentLine):
-        print "Add Text to pil surface"
         self.tmpSize = currentDraw.textsize(currentText, font=currentFont)
         self.tmpWidth = self.GetSizeTuple()[0]/2 - self.tmpSize[0]/2
         self.tmpHeight = self.GetSizeTuple()[1]/2 - self.tmpSize[1]/2 - 50 + (25 * currentLine)
         currentDraw.text((self.tmpWidth, self.tmpHeight), currentText, font=currentFont, fill=215)
 
-    def BlitTextSurface(self, currentPanel, currentPIL):
-        print "display text surface to screen"
-        self.ShowBitmapFromPIL(currentPanel, currentPIL)
+    def BlitTextSurface(self, currentPanel, currentPIL, currentBmp):
+        self.ShowBitmapFromPIL(currentPIL, currentBmp)
         currentPanel.SetFocus()
 
-    def ShowBitmapFromPIL(self, currentPanel, currentPIL): # when we operate on the image...
+    def ShowBitmapFromPIL(self, currentPIL, currentBmp): # when we operate on the image...
         self.wxImg = wx.EmptyImage(currentPIL.size[0], currentPIL.size[1])
         self.wxImg.SetData(currentPIL.convert("RGB").tostring())
         self.wxImg.SetAlphaData(currentPIL.convert("RGBA").tostring()[3::4])
         self.bmpImg = self.wxImg.ConvertToBitmap()
-        currentPanel.DestroyChildren()
-        if self.bmp: self.bmp.Destroy()
-        self.bmp = wx.StaticBitmap(currentPanel, -1, self.bmpImg, wx.Point(0,0), wx.Size(currentPIL.size[0], currentPIL.size[1]))
+        currentBmp.SetBitmap(self.bmpImg)
