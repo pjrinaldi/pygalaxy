5985e7586e5d941a64c24fe3bfe55485bf5fbc5e
Pasquale Rinaldi <pjrinaldi@gmail.com>
2012-12-07 08:04:44 -0500

    set background color

--- GalaxyFrame.py
+++ GalaxyFrame.py
@@ -4,5 +4,6 @@ class GalaxyFrame(wx.Frame):
     
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(800, 600))
+        self.SetBackgroundColour(wx.Colour(0, 0, 0))
         
         self.Show(True)
