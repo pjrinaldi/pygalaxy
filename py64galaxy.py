81b702003a9066e2bc36e7edd081f8a606ff583d
Pasquale Rinaldi <pjrinaldi@gmail.com>
2013-03-30 16:49:35 -0400

    got the damn code completion module error removed.  now once i have a charge, i can start working on the code again.

--- py64galaxy.py
+++ py64galaxy.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 # import statements
+import sys
+
+print sys.path
 import pygame, random, sys
 
 # from statements
@@ -26,6 +29,7 @@ def drawText(text, font, surface, x, y):
 
 pygame.init()
 mainClock = pygame.time.Clock()
+
 '''
     self.titleFont = ImageFont.truetype("./resources/C64_Pro_Mono_v1.0-STYLE.ttf", 48)
     self.gameFont = ImageFont.truetype("./resources/C64_Pro_Mono_v1.0-STYLE.ttf", 12)
